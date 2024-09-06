a1360165abd2eb69727459cf67b359c21fff7e3f
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-09-05 22:32:57 -0600

    added a title and description UI to create todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,20 @@ export default function Home() {
   const [todos, setTodos] = useState<TodoItem[]>([
     {title: "Example 1", description: "This is an example", completed: false}
   ]);
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // so the page doesnt refresh
+    setTodos(prev => {
+      const newTodos = [...prev];
+      newTodos.push({title, description, completed: false});
+      return newTodos;
+    });
+    setTitle("");
+    setDescription("");
+  };
   return (
     <div className="max-w-screen-md mx-auto p-4">
       <h1 className="text-xl font-bold">
@@ -27,9 +41,21 @@ export default function Home() {
             const newTodos = [...prev];
             newTodos[index].completed = e.target.checked;
             return newTodos;
-        })}></input><span className="font-semibold">{title}</span>{description}</li>
+        })}
+        />
+        <span className="font-semibold">{title}</span>
+          {description}
+        </li>
       ))}
       </ul>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" id="title" value={title} onChange={e => setTitle(e.target.value)}/>
+        <label htmlFor="description">Description</label>
+        <input type="text" name="description" id="description" value={description} onChange={e => setDescription(e.target.value)}/>
+        <button type="submit">Create</button>
+      </form>
     </div>
   );
 }
