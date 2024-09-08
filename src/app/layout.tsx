99c04c245724a5da21d0e331d4c0f7c702622287
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-09-08 11:17:43 -0600

    added clerk provider and convex providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ConvextClientProvider } from "./convex-client-provider";
+import { Providers } from "./providers";
 
 
 export const metadata: Metadata = {
@@ -16,9 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ConvextClientProvider>
+        <Providers>
           {children}
-        </ConvextClientProvider>
+        </Providers>
       </body>
     </html>
   );
