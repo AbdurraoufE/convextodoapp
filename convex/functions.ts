5a1656b9a9fe32ec9e291303e5ac9d5b19a9f957
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-09-08 13:14:31 -0600

    Added a generate Todos with LLM

--- convex/functions.ts
+++ convex/functions.ts
@@ -1,6 +1,7 @@
 import { mutation, query } from "./_generated/server";
 import {v} from "convex/values"
 import { requireUser } from "./helpers";
+import { internalMutation } from "./_generated/server";
 
 export const listTodos = query({
     handler: async (ctx) => {
@@ -53,5 +54,22 @@ export const deleteTodo = mutation({
             throw new Error("Unauthorized access to update")
         }
         await ctx.db.delete(args.id);
+    }
+});
+
+export const createManyTodos = internalMutation({
+    args: {
+        userId: v.string(),
+        todos: v.array(v.object({title: v.string(), description: v.string()}))
+    },
+    handler: async (ctx, args) => {
+        for (const todo of args.todos){
+            await ctx.db.insert("todos", {
+                title: todo.title,
+                description: todo.description,
+                completed: false,
+                userId: args.userId
+            })
+        }
     }
 });
